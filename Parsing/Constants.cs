b5e8eac4a0f97e9eb6cfbe64ab0909b47a39c947
videojuegos.eui-vi <videojuegos.eui-vi@hotmail.com>
2019-02-11 13:31:03 +0100

    Added regularExpression Insert

--- Parsing/Constants.cs
+++ Parsing/Constants.cs
@@ -8,7 +8,8 @@ namespace Parsing
 {
     class Constants
     {
-        String regEXSelect = @"SELECT\s+(\*|\w+)\s+FROM\s+(\w+)\s+WHERE\s+(\w+<[0-9]+|\w+>[0-9]+|\w+=[0-9]+)";
-        String regExDelete = @"DELETE\s+FROM\s+(\w+)\s+WHERE\s+(\w+<[0-9]+|\w+>[0-9]+|\w+=[0-9]+)";
+        String regEXSelect = @"SELECT\s+(\*|\w+)\s+FROM\s+(\w+)\s+WHERE\s+(\w+<[0-9]+|\w+>[0-9]+|\w+=[0-9]+);";
+        String regExDelete = @"DELETE\s+FROM\s+(\w+)\s+WHERE\s+(\w+<[0-9]+|\w+>[0-9]+|\w+=[0-9]+);";
+        String regExInsert = @"INSERT\s+INTO\s+(\w+)\s+VALUES\s+\(([^\)]+)\);";
     }
 }
