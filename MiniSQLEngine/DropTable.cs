98ce638451d790be6ceca87fa20bab099ae5abb8
Leire <liglesias027@ikasle.ehu.eus>
2019-02-14 12:47:28 +0100

    Added the DropTable and CreateTable parsing

--- MiniSQLEngine/DropTable.cs
+++ MiniSQLEngine/DropTable.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class DropTable : Query
+namespace MiniSQLEngine
 {
-    //Overraid.
-	public overraid DropTable(String name_table)
-	{
+    public class DropTable : Query
+    {
+        String tableName;
 
-	}
+        public DropTable(String table)
+        {
+            tableName = table;
+        }
+        public override void Run()
+        {
+
+        }
+    }
 }
--- Parsing/Class1.cs
+++ Parsing/Class1.cs
@@ -11,25 +11,69 @@ namespace Parsing
 {
     public class Class1
     {
-        public void parse(String pQuery)
+        public void Parse(String pQuery)
         {
             Match matchselect = Regex.Match(pQuery,Constants.regExSelect);
             if (matchselect.Success)
             {
-                manageSelect(pQuery);
+                ManageSelect(pQuery);
+            }
+
+            Match matchDropTable = Regex.Match(pQuery, Constants.regExTypesDropTable);
+            else if (matchDropTable.Success)
+            {
+                ManageDropTable(pQuery);
+            }
+
+            Match matchCreateTable = Regex.Match(pQuery, Constants.regExTypesCreateTable);
+            else if (matchCreateTable.Success)
+            {
+                ManageCreateTable(pQuery);
             }
 
 
-          
+
         }
 
        
 
-        public Query manageSelect(String pQuery)
+
+        public Query ManageSelect(String pQuery)
         {
             ClassSelect query = new ClassSelect();
             return query;
-      
+
+        }
+
+        public Query ManageDropTable(String pQuery)
+        {
+            Match matchDropTable = Regex.Match(pQuery, Constants.regExpDropTable);
+            if (matchDropTable.Success)
+            {
+                DropTable query = new DropTable();
+                return query;
+            }
+            else
+            {
+                return null;
+            }
+
+
+        }
+
+        public Query ManageCreateTable(String pQuery)
+        {
+            Match matchCreateTable = Regex.Match(pQuery, Constants.regExpCreateTable);
+            if (matchCreateTable.Success)
+            {
+                ClassTable query = new ClassTable();
+                return query;
+            }
+            else
+            {
+                return null;
+            }
+
         }
     }
 }
