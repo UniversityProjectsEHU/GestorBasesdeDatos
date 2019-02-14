756d2101a3b06bb015b00b314c28540c1c43aabf
JosuGom3z <josu1398@gmail.com>
2019-02-14 13:11:28 +0100

    Adding ClassInsert with the edited constructor

--- Parsing/Class1.cs
+++ Parsing/Class1.cs
@@ -14,11 +14,16 @@ namespace Parsing
         public void parse(String pQuery)
         {
             Match matchselect = Regex.Match(pQuery,Constants.regExSelect);
+            Match matchinsert = Regex.Match(pQuery, Constants.regExTypeInsert);
             if (matchselect.Success)
             {
                 manageSelect(pQuery);
             }
 
+            else if (matchinsert.Success)
+            {
+                ManageInsert(pQuery);
+            }
 
           
         }
@@ -30,6 +35,22 @@ namespace Parsing
             ClassSelect query = new ClassSelect();
             return query;
       
+        }
+
+        public Query ManageInsert(String pQuery)
+        {
+            //public const String regExInsert = @"INSERT\s+INTO\s+(\w+)\s+VALUES\s+\(([^\)]+)\);";
+            Match match = Regex.Match(pQuery, Constants.regExInsert);
+            if (match.Success)
+            {
+                string table = match.Groups[1].Value;
+                string values = match.Groups[2].Value;
+                string[] myArray = values.Split(',');
+                ClassInsert query = new ClassInsert(table, myArray);
+                return query;
+            }
+            
+            return null;
         }
     }
 }
