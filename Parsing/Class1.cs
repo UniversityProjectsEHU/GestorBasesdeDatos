3a9af36717b8b15a8403dbfd283ab49707581279
videojuegos.eui-vi <videojuegos.eui-vi@hotmail.com>
2019-02-14 13:01:11 +0100

    Delete Parsing done

--- Parsing/Class1.cs
+++ Parsing/Class1.cs
@@ -18,12 +18,47 @@ namespace Parsing
             {
                 manageSelect(pQuery);
             }
+            Match matchdelete = Regex.Match(pQuery, Constants.regExTypeDelete);
+             else if (matchdelete.Success)
+            {
+                ManageDelete(pQuery);
+            }
+            Match matchupdate = Regex.Match(pQuery, Constants.regExTypeUpdate);
+            else if (matchselect.Success)
+            {
+                ManageUpdate(pQuery);
+            }
 
 
-          
         }
 
-       
+        public Query ManageUpdate(string pQuery)
+        {
+            Match Update = Regex.Match(pQuery, Constants.regExpUpdate);
+            if(Update.Success)
+            {
+                String Columna = Update.Groups[0].Value;
+                String Tabla = Update.Groups[1].Value;
+                String Condicion = Update.Groups[2].Value;
+                ClassUpdate query = new ClassUpdate();
+
+            }
+            return query;
+        }
+
+        public Query ManageDelete(string pQuery)
+        {
+            Match Update = Regex.Match(pQuery, Constants.regExpUpdate);
+            if (Update.Success)
+            {
+                String Columna = Update.Groups[1].Value;
+                String Tabla = Update.Groups[2].Value;
+                String Condicion = Update.Groups[3].Value;
+                ClassUpdate query = new ClassUpdate();
+
+            }
+            return query;
+        }
 
         public Query manageSelect(String pQuery)
         {
