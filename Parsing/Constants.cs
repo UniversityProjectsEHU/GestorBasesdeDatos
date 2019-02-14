908f499a4358211319f9a49623945a320d535e71
Sergio Navarro <sernavarrops3@gmail.com>
2019-02-14 11:52:28 +0100

    Expresions create table, database and drop

--- Parsing/Constants.cs
+++ Parsing/Constants.cs
@@ -8,12 +8,13 @@ namespace Parsing
 {
     class Constants
     {
-        String regEXSelect = @"SELECT\s+(\*|\w+)\s+FROM\s+(\w+)\s+WHERE\s+(\w+<[0-9]+|\w+>[0-9]+|\w+=[0-9]+);";
+        String regExSelect = @"SELECT\s+(\*|\w+)\s+FROM\s+(\w+)\s+WHERE\s+(\w+<[0-9]+|\w+>[0-9]+|\w+=[0-9]+);";
         String regExDelete = @"DELETE\s+FROM\s+(\w+)\s+WHERE\s+(\w+<[0-9]+|\w+>[0-9]+|\w+=[0-9]+);";
         String regExInsert = @"INSERT\s+INTO\s+(\w+)\s+VALUES\s+\(([^\)]+)\);";
-        String regEXSelect = @"SELECT\s+(\*|\w+)\s+FROM\s+(\w+)\s+WHERE\s+(\w+<[0-9]+|\w+>[0-9]+|\w+=[0-9]+)";
-        String regExDelete = @"DELETE\s+FROM\s+(\w+)\s+WHERE\s+(\w+<[0-9]+|\w+>[0-9]+|\w+=[0-9]+)";
-        string regExp = @"UPDATE\s+(\w+)\s+SET\s+([^ WHERE]+)\s+WHERE\s+(\w+>[0-9]+|\w+<[0-9]+|\w+=[0-9]+);";
+        String regExpUpdate = @"UPDATE\s+(\w+)\s+SET\s+([^ WHERE]+)\s+WHERE\s+(\w+>[0-9]+|\w+<[0-9]+|\w+=[0-9]+);";
+        String regExpCreateDatabase = @"CREATE DATABASE(\w+);";
+        String regExpDropDatabase = @"DROP DATABASE(\w+);";
+        String regExpCreateTable = @"CREATE TABLE(\w+) \(([^\)]+)\);";
         String regExTypeSelect = @"(SELECT)";
         String regExTypeInsert = @"(INSERT)";
         String regExTypeUpdate = @"(UPDATE)";
