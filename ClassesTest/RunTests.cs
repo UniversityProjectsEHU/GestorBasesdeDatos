d64281ff7004ffd21b736c5579a44bb2dbdc3fcd
JosuGom3z <josu1398@gmail.com>
2019-03-04 10:51:52 +0100

    commited, create database and create table on going

--- ClassesTest/RunTests.cs
+++ ClassesTest/RunTests.cs
@@ -17,6 +17,7 @@ namespace ClassesTest
             string myTable = "thisTable";
             string[] values = { "One String true", "Two String false", "Three String false", "Caramba String false" };
             string[] valuesToInsert = { "One", "Two", "Three", "Caramba" };
+            string pathfileDEF = @"..//..//..//data//" + dbname + "//" + myTable + ".def";
             /*string queryCreateDB = @"CREATE DATABASE myDB;";
             string queryCreateTable = @"CREATE TABLE myTable (column1 int true,column2 string false,column3 int false);";
             string queryInsert = @"INSERT INTO table1 VALUES (10,abc,8);";
@@ -25,10 +26,10 @@ namespace ClassesTest
             ClassCreateTable newTable = new ClassCreateTable(myTable, values);
             ClassInsert inserted = new ClassInsert(myTable, valuesToInsert);
             newDB.Run(dbname);
-            bool exists = Directory.Exists(@"..//..//..//data//thisTable");
+            bool exists = Directory.Exists(@"..//..//..//data//"+dbname);
             Assert.AreEqual(true, exists);
             newTable.Run(dbname);
-            bool existsTables = Directory.Exists(@"..//..//..//data//myDB//thisTable.data");
+            bool existsTables = File.Exists(@"..//..//..//data//"+dbname+"//"+myTable+".data");
             Assert.AreEqual(true, existsTables);
             inserted.Run(dbname);
             /*string fullPath = @"..//..//..//data//myDB//thisTable.data";
