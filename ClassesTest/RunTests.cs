2314bcedeb17681d642355e0af8622f7e248725c
Sergio Navarro <sernavarrops3@gmail.com>
2019-03-04 12:12:21 +0100

    RunTest with all methods

--- ClassesTest/RunTests.cs
+++ ClassesTest/RunTests.cs
@@ -11,29 +11,12 @@ namespace ClassesTest
     {
         [TestMethod]
         public void TestCreateDropDatabaseTable()
-        public void ExecuteTest()
         {
             string myDB = "myDB";
             ClassCreateDatabase db = new ClassCreateDatabase(myDB);
             db.Run(myDB);
             bool exists = Directory.Exists(@"..//..//..//data//myDB");
             //Testing CreateDatabase
-            string dbname = "myDB";
-            //string dbname1 = "myDB1";
-            string myTable = "thisTable";
-            string[] values = { "One String true", "Two String false", "Three String false", "Caramba String false" };
-            string[] valuesToInsert = { "One", "Two", "Three", "Caramba" };
-            //string pathfileDEF = @"..//..//..//data//" + dbname + "//" + myTable + ".def";
-            string rutaCompleta = @"..//..//..//data//" + dbname + "//" + myTable + ".data";
-            /*string queryCreateDB = @"CREATE DATABASE myDB;";
-            string queryCreateTable = @"CREATE TABLE myTable (column1 int true,column2 string false,column3 int false);";
-            string queryInsert = @"INSERT INTO table1 VALUES (10,abc,8);";
-            */
-            ClassCreateDatabase newDB = new ClassCreateDatabase(dbname);
-            ClassCreateTable newTable = new ClassCreateTable(myTable, values);
-            ClassInsert inserted = new ClassInsert(myTable, valuesToInsert);
-            newDB.Run(dbname);
-            bool exists = Directory.Exists(@"..//..//..//data//" + dbname);
             Assert.AreEqual(true, exists);
 
             string myTable = "myTable";
@@ -61,6 +44,27 @@ namespace ClassesTest
             //Testing DropDatabase
             bool existsDrop = Directory.Exists(@"..//..//..//data//myDB");
             Assert.AreEqual(false, existsDrop);
+        }
+        [TestMethod]
+        public void ExecuteTest()
+        {
+            string dbname = "myDB";
+            //string dbname1 = "myDB1";
+            string myTable = "thisTable";
+            string[] values = { "One String true", "Two String false", "Three String false", "Caramba String false" };
+            string[] valuesToInsert = { "One", "Two", "Three", "Caramba" };
+            //string pathfileDEF = @"..//..//..//data//" + dbname + "//" + myTable + ".def";
+            string rutaCompleta = @"..//..//..//data//" + dbname + "//" + myTable + ".data";
+            /*string queryCreateDB = @"CREATE DATABASE myDB;";
+            string queryCreateTable = @"CREATE TABLE myTable (column1 int true,column2 string false,column3 int false);";
+            string queryInsert = @"INSERT INTO table1 VALUES (10,abc,8);";
+            */
+            ClassCreateDatabase newDB = new ClassCreateDatabase(dbname);
+            ClassCreateTable newTable = new ClassCreateTable(myTable, values);
+            ClassInsert inserted = new ClassInsert(myTable, valuesToInsert);
+            newDB.Run(dbname);
+            bool exists = Directory.Exists(@"..//..//..//data//" + dbname);
+            Assert.AreEqual(true, exists);
             newTable.Run(dbname);
             /*bool existsTables = File.Exists(@"..//..//..//data//"+dbname+"//"+myTable+".data");
             Assert.AreEqual(true, existsTables);
