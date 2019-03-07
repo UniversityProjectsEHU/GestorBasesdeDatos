d683b8ac7c6e70cabda2204ec82163a9fefb13fe
Sergio Navarro <sernavarrops3@gmail.com>
2019-03-07 12:53:15 +0100

    Main console

--- MainConsole/Program.cs
+++ MainConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MiniSQLEngine;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,20 +16,24 @@ namespace MainConsole
             Database db = new Database("usuarios1");
             db.Query("CREATE DATABASE usuarios1;");
             int contador = 1;
+            Console.WriteLine("# Test "+ contador); 
             foreach (string linea in lineas)
             {
                 if (linea != "")
                 {            
+                        
                         db.Query(linea);                      
                 }
                 else
                 {
                     contador = contador + 1;
+                    Console.WriteLine("# Test " + contador);
                     string dbnombre = "usuarios" + contador;
                     db = new Database(dbnombre);
                     db.Query("CREATE DATABASE "+dbnombre+";");
                 }
             }
+            Console.ReadKey(true);
         }
     }
 }
