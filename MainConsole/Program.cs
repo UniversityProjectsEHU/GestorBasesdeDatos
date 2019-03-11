42d94ecd47db80dec0a31c814ee23a7164a0a621
Sergio Navarro <sernavarrops3@gmail.com>
2019-03-11 13:32:44 +0100

    Changes in Program

--- MainConsole/Program.cs
+++ MainConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using MiniSQLEngine;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -13,43 +14,42 @@ namespace MainConsole
     {
         static void Main(string[] args)
         {
-            String[] lineas = System.IO.File.ReadAllLines("..//..//..//data//input.txt");
-            Database db = new Database("usuarios1");
-            db.Query("CREATE DATABASE usuarios1;");
-            int contador = 1;
-            Console.WriteLine("# Test " + contador);
-            string infor = "";
-            foreach (string linea in lineas)
-            {
-                if (linea != "")
+                String[] lineas = System.IO.File.ReadAllLines("..//..//..//data//input.txt");
+                Database db = new Database("usuarios1");
+                db.Query("CREATE DATABASE usuarios1;");
+                int contador = 1;
+                Console.WriteLine("# Test " + contador);
+                string infor = "";
+                ArrayList tiempos = new ArrayList();
+                int totaltime = 0;
+                foreach (string linea in lineas)
                 {
-
-                    Stopwatch tiempo = Stopwatch.StartNew();
-
-                    if (linea.Contains("SELECT"))
+                    if (linea != "")
                     {
+                        Stopwatch tiempo = Stopwatch.StartNew();
                         infor = db.Query(linea);
+                        Console.WriteLine(infor + " " + tiempo.ElapsedMilliseconds.ToString() + "ms");
+                        tiempos.Add(tiempo.ElapsedMilliseconds);
                     }
                     else
                     {
-                        infor = db.Query(linea);
+                        for (int i = 0; i < tiempos.Count; i++)
+                        {
+                            int eltime = Convert.ToInt16(tiempos[i]);
+                            totaltime = totaltime + eltime;
+                        }
+                        Console.WriteLine("TOTAL TIME: " + totaltime);
+                        contador = contador + 1;
+                        Console.WriteLine("");
+                        Console.WriteLine("# Test " + contador);
+                        string dbnombre = "usuarios" + contador;
+                        db = new Database(dbnombre);
+                        db.Query("CREATE DATABASE " + dbnombre + ";");
+                        tiempos.Clear();
+                        totaltime = 0;
                     }
-                    Console.WriteLine(infor + " " + tiempo.ElapsedMilliseconds.ToString() + "ms");
-                }
-                else
-                {
-                    contador = contador + 1;
-                    Console.WriteLine("");
-                    Console.WriteLine("# Test " + contador);
-                    string dbnombre = "usuarios" + contador;
-                    db = new Database(dbnombre);
-                    db.Query("CREATE DATABASE " + dbnombre + ";");
-                }
             }
-            Console.ReadKey(true);
-
-            Console.WriteLine(db.Query("SELECT Name,Age FROM MyTable WHERE Age=18;"));
-            Console.ReadKey(true);
-        }
+                Console.ReadKey(true);
+            }
     }
 }
