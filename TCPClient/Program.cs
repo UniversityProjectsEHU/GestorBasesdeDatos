d8656fcbe0a498e5edb658e29c21d01f5ff9d8cc
Sergio Hernando <sergiohg10@hotmail.es>
2019-05-06 12:01:02 +0200

    Uploaded Client and server

---
+++ TCPClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace TCPClientExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string argPrefixIp = "ip=";
+            const string argPrefixPort = "port=";
+
+            string ip = null;
+            int port = 0;
+            foreach (string arg in args)
+            {
+                if (arg.StartsWith(argPrefixIp)) ip = arg.Substring(argPrefixIp.Length);
+                else if (arg.StartsWith(argPrefixPort)) port = int.Parse(arg.Substring(argPrefixPort.Length));
+            }
+            if (ip == null || port == 0)
+            {
+                Console.WriteLine("ERROR. Usage: TCPClient ip=<ip> port=<port>");
+                return;
+            }
+
+            using (TcpClient client = new TcpClient(ip, port))
+            {
+                NetworkStream networkStream = client.GetStream();
+
+                byte[] outputBuffer = Encoding.ASCII.GetBytes("Do you want to marry me????");
+                byte[] inputBuffer = new byte[1024];
+                byte[] endMessage = Encoding.ASCII.GetBytes("END");
+
+                for (int i = 0; i < 5; i++)
+                {
+                    networkStream.Write(outputBuffer, 0, outputBuffer.Length);
+
+                    int readBytes = networkStream.Read(inputBuffer, 0, 1024);
+                    Console.WriteLine("Response received: " + Encoding.ASCII.GetString(inputBuffer,0,readBytes));
+
+                    Thread.Sleep(2000);
+                }
+                networkStream.Write(endMessage, 0, endMessage.Length);
+            }
+        }
+    }
+}
---
+++ TCPServer/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCPServerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string argPrefixPort = "port=";
+
+            int port = 0;
+            foreach (string arg in args)
+            {
+                if (arg.StartsWith(argPrefixPort)) port = int.Parse(arg.Substring(argPrefixPort.Length));
+            }
+            if (port == 0)
+            {
+                Console.WriteLine("ERROR. Usage: TCPClient ip=<ip> port=<port>");
+                return;
+            }
+
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+
+            Console.WriteLine("Server listening for clients");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Client connection accepted");
+
+                var childSocketThread = new Thread(() =>
+                {
+                    byte[] inputBuffer = new byte[1024];
+
+                    NetworkStream networkStream = client.GetStream();
+
+                    //Read message from the client
+                    int size = networkStream.Read(inputBuffer, 0, 1024);
+                    string request = Encoding.ASCII.GetString(inputBuffer, 0, size);
+
+                    while (request != "END")
+                    {
+                        Console.WriteLine("Request received: " + request);
+
+                        byte[] outputBuffer = Encoding.ASCII.GetBytes("My answer is NO");
+                        networkStream.Write(outputBuffer, 0, outputBuffer.Length);
+
+                        size = networkStream.Read(inputBuffer, 0, 1024);
+                        request = Encoding.ASCII.GetString(inputBuffer, 0, size);
+                    }
+                    client.Close();
+                });
+                childSocketThread.Start();
+            }
+        }
+    }
+}
